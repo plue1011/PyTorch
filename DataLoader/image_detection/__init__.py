725001a29fba5ba33d4d17133f2ba9a28e54e9fb
plue1011 <fuekimasao@yahoo.co.jp>
2020-08-12 23:38:52 +0900

    first commit

---
+++ DataLoader/image_detection/__init__.py
@@ -0,0 +1,2 @@
+from image_detection.make_dataloader import *
+from image_detection.data_augmentation import *
--- DataLoader/preprocessing/__init__.py
+++
@@ -1,2 +0,0 @@
-from preprocessing.make_dataloader import *
-from preprocessing.data_augmentation import *
